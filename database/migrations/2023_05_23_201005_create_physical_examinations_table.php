56870d8c0f75025b8854f140bb8a992ae4f058cf
Churchill Capagngan <cbcapagngan@student.apc.edu.ph>
2023-05-26 23:53:15 +0800

    undone might do on school after review

---
+++ database/migrations/2023_05_23_201005_create_physical_examinations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('physical_examinations', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->integer('medical_exam_id')->unsigned();
+            $table->timestamps();
+
+            //Foreign keys
+            $table->foreign('medical_exam_id')->references('id')->on('medical_exams')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('physical_examinations');
+    }
+};
