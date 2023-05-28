eaf9b626ecc24fdfa89dbd6b45156b5430fc1866
DaikuSumairu <daikusumairu84@gmail.com>
2023-05-28 15:36:59 +0800

    creating consultation

---
+++ resources/views/nurse/record/consultation/create.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Consultation')
+
+@section('content_header')
+    <h1>Creating Medical Exam</h1>
+@stop
+
+@section('content')
+    
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    
+@stop
