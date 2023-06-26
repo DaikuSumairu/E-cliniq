42f9e7337b3e1bb6bedaa667c3079ec14a2de95c
Churchill Capagngan <cbcapagngan@student.apc.edu.ph>
2023-06-26 20:47:22 +0800

    target found when click the content in calendar

--- resources/views/nurse/appointment/index.blade.php
+++ resources/views/nurse/appointment/index.blade.php
@@ -121,71 +121,73 @@
                 <div class="col" style="height: 415px;overflow: auto;">
                     <div class="col" style="width: 380px; border: 1px;">
                         @foreach ($appointments as $appointment)
-                            @if($appointment->status == 'Pending')
-                                <div class="container my-3 border">
-                                    <div class="row mt-2" style="width: 380px;">
-                                        <div class="col">
-                                            <label><strong>Name: </strong></label> {{ $appointment->name }}<br>
-                                            <label><strong>ID: </strong></label> {{ $appointment->school_id }}<br>
-                                            <label><strong>Status: </strong></label> {{ $appointment->status }}<br>
-                                        </div>
-                                        <div class="col">
-                                            <label><strong>Date: </strong></label> {{ $appointment->date }}<br>
-                                            <label><strong>Start Time: </strong></label> {{ $appointment->start_time }}<br>
-                                            <label><strong>End Time: </strong></label> {{ $appointment->end_time }}
-                                        </div>
-                                    </div>
-                                    <div class="row mt-1 mb-2">
-                                        <div class="col">
-                                            <label><strong>Reason:</strong></label>
-                                            <textarea class="form-control" readonly>{{ $appointment->reason }}</textarea>
-                                        </div>
-                                    </div>
-                                    <div class="row mb-2">
-                                        <div class="col text-right">
-                                            <form method="POST" action="{{ route('nurse.appointmentUpdate', $appointment->id) }}" onsubmit="return confirm('Are you sure you want to accept this appointment?');">
-                                                @csrf
-                                                @method('PUT')
-                                                <input type="hidden" name="status" value="Accept">
-                                                <button type="submit" class="btn btn-success">Accept</button>
-                                            </form>
-                                        </div>
-                                        <div class="col-0 text-right">
-                                            <form method="POST" action="{{ route('nurse.appointmentDestroy', $appointment->id) }}" onsubmit="return confirm('Are you sure you want to decline this appointment?');">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-danger mx-2">Decline</button>
-                                            </form>
-                                        </div>
-                                    </div>
+                        @if($appointment->status == 'Pending')
+                        <div id="appointment-{{ $appointment->id }}" class="container my-3 border">
+                            <!-- Add the id attribute to the appointment container -->
+                            <div class="row mt-2" style="width: 380px;">
+                                <div class="col">
+                                    <label><strong>Name: </strong></label> {{ $appointment->name }}<br>
+                                    <label><strong>ID: </strong></label> {{ $appointment->school_id }}<br>
+                                    <label><strong>Status: </strong></label> {{ $appointment->status }}<br>
                                 </div>
-                            @else
-                                <div class="container my-3 border">
-                                    <div class="row mt-2" style="width: 380px;">
-                                        <div class="col">
-                                            <label><strong>Name: </strong></label> {{ $appointment->name }}<br>
-                                            <label><strong>ID: </strong></label> {{ $appointment->school_id }}<br>
-                                            <label><strong>Status: </strong></label><span style="color: green;"> {{ $appointment->status }}</span><br>
-                                        </div>
-                                        <div class="col">
-                                            <label><strong>Date: </strong></label> {{ $appointment->date }}<br>
-                                            <label><strong>Start Time: </strong></label> {{ $appointment->start_time }}<br>
-                                            <label><strong>End Time: </strong></label> {{ $appointment->end_time }}
-                                        </div>
-                                    </div>
-                                    <div class="row mt-1 mb-2">
-                                        <div class="col">
-                                            <label><strong>Reason:</strong></label>
-                                            <textarea class="form-control" readonly>{{ $appointment->reason }}</textarea>
-                                        </div>
-                                    </div>
-                                    <div class="row mb-2">
-                                        <div class="col text-right">
-                                            <a class="btn btn-info" href="#">Done</a>
-                                        </div>
-                                    </div>
+                                <div class="col">
+                                    <label><strong>Date: </strong></label> {{ $appointment->date }}<br>
+                                    <label><strong>Start Time: </strong></label> {{ $appointment->start_time }}<br>
+                                    <label><strong>End Time: </strong></label> {{ $appointment->end_time }}
                                 </div>
-                            @endif
+                            </div>
+                            <div class="row mt-1 mb-2">
+                                <div class="col">
+                                    <label><strong>Reason:</strong></label>
+                                    <textarea class="form-control" readonly>{{ $appointment->reason }}</textarea>
+                                </div>
+                            </div>
+                            <div class="row mb-2">
+                                <div class="col text-right">
+                                    <form method="POST" action="{{ route('nurse.appointmentUpdate', $appointment->id) }}" onsubmit="return confirm('Are you sure you want to accept this appointment?');">
+                                        @csrf
+                                        @method('PUT')
+                                        <input type="hidden" name="status" value="Accept">
+                                        <button type="submit" class="btn btn-success">Accept</button>
+                                    </form>
+                                </div>
+                                <div class="col-0 text-right">
+                                    <form method="POST" action="{{ route('nurse.appointmentDestroy', $appointment->id) }}" onsubmit="return confirm('Are you sure you want to decline this appointment?');">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger mx-2">Decline</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        @else
+                        <div id="appointment-{{ $appointment->id }}" class="container my-3 border">
+                            <!-- Add the id attribute to the appointment container -->
+                            <div class="row mt-2" style="width: 380px;">
+                                <div class="col">
+                                    <label><strong>Name: </strong></label> {{ $appointment->name }}<br>
+                                    <label><strong>ID: </strong></label> {{ $appointment->school_id }}<br>
+                                    <label><strong>Status: </strong></label><span style="color: green;"> {{ $appointment->status }}</span><br>
+                                </div>
+                                <div class="col">
+                                    <label><strong>Date: </strong></label> {{ $appointment->date }}<br>
+                                    <label><strong>Start Time: </strong></label> {{ $appointment->start_time }}<br>
+                                    <label><strong>End Time: </strong></label> {{ $appointment->end_time }}
+                                </div>
+                            </div>
+                            <div class="row mt-1 mb-2">
+                                <div class="col">
+                                    <label><strong>Reason:</strong></label>
+                                    <textarea class="form-control" readonly>{{ $appointment->reason }}</textarea>
+                                </div>
+                            </div>
+                            <div class="row mb-2">
+                                <div class="col text-right">
+                                    <a class="btn btn-info" href="#">Done</a>
+                                </div>
+                            </div>
+                        </div>
+                        @endif
                         @endforeach
                     </div>
                 </div>
@@ -213,57 +215,87 @@
 
     <!-- script for calendar -->
     <script>
-        document.addEventListener('DOMContentLoaded', function() {
-        var calendarEl = document.getElementById('calendar');
-        var calendar = new FullCalendar.Calendar(calendarEl, {
-            initialView: 'timeGridWeek', // Change to timeGridWeek for vertical week view
-            nowIndicator: true, // Enable the now indicator to track the current time
-            hiddenDays: [0, 6], // Hide Sunday (0) and Saturday (6)
-            allDaySlot: false, // Disable the "all-day" section
-            slotMinTime: '09:00:00', // Set the minimum time to 9:00 AM
-            slotMaxTime: '17:30:00', // Set the maximum time to 5:30 PM
-            slotDuration: '00:15:00', // Set the slot duration to 15 minutes (adjust as needed)
-            slotLabelInterval: '01:00', // Show slot labels every hour
-            businessHours: [
-                {
-                    daysOfWeek: [1, 2, 3, 4, 5], // Monday - Friday
-                    startTime: '09:00', // 9:00 AM
-                    endTime: '12:00' // 12:00 PM
-                },
-                {
-                    daysOfWeek: [1, 2, 3, 4, 5], // Monday - Friday
-                    startTime: '13:00', // 1:00 PM
-                    endTime: '17:30' // 5:30 PM
-                }
-            ]
-        });
-        calendar.render();
-    
-        @foreach ($appointed as $appointedones)
+        document.addEventListener('DOMContentLoaded', function () {
+            var calendarEl = document.getElementById('calendar');
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'timeGridWeek', // Change to timeGridWeek for vertical week view
+                nowIndicator: true, // Enable the now indicator to track the current time
+                hiddenDays: [0, 6], // Hide Sunday (0) and Saturday (6)
+                allDaySlot: false, // Disable the "all-day" section
+                slotMinTime: '09:00:00', // Set the minimum time to 9:00 AM
+                slotMaxTime: '17:30:00', // Set the maximum time to 5:30 PM
+                slotDuration: '00:15:00', // Set the slot duration to 15 minutes (adjust as needed)
+                slotLabelInterval: '01:00', // Show slot labels every hour
+                businessHours: [
+                    {
+                        daysOfWeek: [1, 2, 3, 4, 5], // Monday - Friday
+                        startTime: '09:00', // 9:00 AM
+                        endTime: '12:00' // 12:00 PM
+                    },
+                    {
+                        daysOfWeek: [1, 2, 3, 4, 5], // Monday - Friday
+                        startTime: '13:00', // 1:00 PM
+                        endTime: '17:30' // 5:30 PM
+                    }
+                ]
+            });
+            calendar.render();
+
+            @foreach ($appointed as $appointedones)
             @if($appointedones->status == "Pending")
-                calendar.addEvent({
+            calendar.addEvent({
                 title: '{{ $appointedones->reason }}',
                 start: '{{ $appointedones->date }}T{{ $appointedones->start_time }}',
                 end: '{{ $appointedones->date }}T{{ $appointedones->end_time }}',
                 color: 'gray',
                 extendedProps: {
+                    id: '{{ $appointedones->id }}', // Add the appointment's id as an extended property
                     name: '{{ $appointedones->name }}',
                     schoolId: '{{ $appointedones->school_id }}'
                 }
-                });
+            });
             @else
-                calendar.addEvent({
+            calendar.addEvent({
                 title: '{{ $appointedones->reason }}',
                 start: '{{ $appointedones->date }}T{{ $appointedones->start_time }}',
                 end: '{{ $appointedones->date }}T{{ $appointedones->end_time }}',
                 color: 'green',
                 extendedProps: {
+                    id: '{{ $appointedones->id }}', // Add the appointment's id as an extended property
                     name: '{{ $appointedones->name }}',
                     schoolId: '{{ $appointedones->school_id }}'
                 }
-                });
+            });
             @endif
-        @endforeach
+            @endforeach
+
+            calendar.on('eventClick', function (info) {
+                var appointmentId = info.event.extendedProps.id; // Get the appointment's id
+                var appointmentContainer = document.getElementById('appointment-' + appointmentId); // Locate the corresponding appointment container in the list
+
+                if (!appointmentContainer) {
+                    // Appointment container not found on the current page
+                    var currentPage = {{ $appointments->currentPage() }}; // Get the current page number
+                    var targetPage = {{ $appointments->lastPage() }}; // Get the last page number
+
+                    if (currentPage !== targetPage) {
+                        var url = '{{ $appointments->url(0) }}'; // Get the base URL for the pagination links
+                        var targetUrl = url.replace(/page=\d+/, 'page=' + targetPage); // Replace the page number in the URL with the target page
+
+                        // Navigate to the target page
+                        window.location.href = targetUrl;
+                    } else {
+                        // Navigate to the first page
+                        window.location.href = '{{ $appointments->url(1) }}';
+                    }
+                } else {
+                    appointmentContainer.scrollIntoView({ behavior: 'smooth' }); // Scroll to the appointment container
+                    appointmentContainer.style.backgroundColor = 'yellow'; // Highlight the appointment container
+                    setTimeout(function () {
+                        appointmentContainer.style.backgroundColor = ''; // Remove the highlighting after a short delay
+                    }, 2000);
+                }
+            });
         });
     </script>
 
